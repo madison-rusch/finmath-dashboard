5177bb291634b36960fced23c2dfa48d9a9a1a5d
Madison Rusch <55900572+madison-rusch@users.noreply.github.com>
2023-05-17 17:20:28 -0500

    fix app py

--- app.py
+++ app.py
@@ -2,70 +2,43 @@ from dash import Dash, html, dcc, dash_table, Input, Output
 import pandas as pd
 import dash
 import sys
+from components import navbar
 import dash_bootstrap_components as dbc
 sys.path.append('..')
-    
-# # app = dash.Dash(
-# #     external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME]
-# # )
-
-# # sidebar = html.Div(
-# #     children = [
-# #         html.H2("Filters"),
-# #         html.Hr(),
-# #         html.P(
-# #             "A simple sidebar layout with filters", className="lead"
-# #         ),
-# #         dbc.Nav(
-# #             [
-# #                 dcc.Dropdown(id = 'one'),
-# #                 html.Br(),
-# #                 dcc.Dropdown(id = 'two'),
-# #                 html.Br(),
-# #                 dcc.Dropdown(id = 'three')
-
-# #             ],
-# #             vertical=True,
-# #             pills=True,
-# #         ),
-# #     ]
-# #     # style=SIDEBAR_STYLE,
-# # )
-
-
-
-# app.layout = html.Div(children = [
-#     dbc.Row(navbar),
-#     dbc.Row([
-#             dbc.Col(sidebar),
-#             dbc.Col()])
-#     ]
-
-    
-    
-#     # [
-# 	# html.H1('FinMath Portfolio Dashboard'),
-
-#     # html.Div(
-#     #     [
-#     #         html.Div(
-#     #             dcc.Link(
-#     #                 f"{page['name']} - {page['path']}", href=page["relative_path"]
-#     #             )
-#     #         )
-#     #         for page in dash.page_registry.values()
-#     #     ]
-#     # ),
-
-# 	# dash.page_container
-# #  ]
-# )
-
-# if __name__ == '__main__':
-# 	app.run_server(debug=True)
 
 
 app = dash.Dash(__name__, 
                 external_stylesheets=[dbc.themes.BOOTSTRAP], 
                 meta_tags=[{"name": "viewport", "content": "width=device-width"}],
                 suppress_callback_exceptions=True)
+
+# Define the navbar
+nav = navbar.Navbar()
+
+# Define the index page layout
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    nav, 
+    html.Div(id='page-content', children=[]), 
+])
+
+
+# Connect to your app pages
+from pages import home, FINM350, FINM367
+
+# Create the callback to handle mutlipage inputs
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/':
+        return home.layout
+    if pathname == '/FINM367':
+        return FINM367.layout
+    if pathname == '/FINM350':
+        return FINM350.layout
+    else: # if redirected to unknown link
+        return "404 Page Error! Please choose a link"
+
+# Run the app on localhost:8050
+if __name__ == '__main__':
+    app.run_server(debug=True)
