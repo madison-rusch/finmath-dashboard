4b63cca5e70066fa0608b1be42b6cecaf9999b7a
Madison Rusch <55900572+madison-rusch@users.noreply.github.com>
2023-05-22 18:47:35 -0500

    Static navigation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, dash_table, Input, Output
+from dash import Dash, html, dcc, dash_table, Input, Output, State
 import pandas as pd
 import dash
 import sys
@@ -7,6 +7,7 @@ import dash_bootstrap_components as dbc
 sys.path.append('..')
 
 # Github Auth token: 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
+# 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
 
 app = dash.Dash(__name__, 
                 external_stylesheets=[dbc.themes.BOOTSTRAP], 
@@ -16,29 +17,57 @@ app = dash.Dash(__name__,
 # Define the navbar
 nav = navbar.Navbar()
 
+# Connect to your app pages
+from pages import home, FINM350, FINM367
+
 # Define the index page layout
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     nav, 
     html.Div(id='page-content', children=[]), 
+    dcc.Store(id='home-layout', data=[home.layout]),
+    dcc.Store(id='FINM350-layout', data=[FINM350.layout]),
+    dcc.Store(id='FINM367-layout', data=[FINM367.layout]),
 ])
 
 
-# Connect to your app pages
-from pages import home, FINM350, FINM367
 
 # Create the callback to handle mutlipage inputs
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/':
-        return home.layout
-    if pathname == '/FINM367':
-        return FINM367.layout
-    if pathname == '/FINM350':
-        return FINM350.layout
-    else: # if redirected to unknown link
-        return "404 Page Error! Please choose a link"
+# @app.callback(Output('page-content', 'children'),
+#               [Input('url', 'pathname')])
+# def display_page(pathname):
+#     if pathname == '/':
+#         return home.layout
+#     if pathname == '/FINM367':
+#         return FINM367.layout
+#     if pathname == '/FINM350':
+#         return FINM350.layout
+#     else: # if redirected to unknown link
+#         return "404 Page Error! Please choose a link"
+    
+app.clientside_callback(
+    """
+    function(pathname, home, FINM367, FINM350) {
+        if (pathname == '/') {
+            return home;
+        }
+        if (pathname == '/FINM367') {
+            return FINM367;
+        }
+        if (pathname == '/FINM350') {
+            return FINM350;
+        }
+        else { 
+            return "404 Page Error! Please choose a link";
+        }
+    }
+    """,
+    Output('page-content', 'children'),
+    Input('url', 'pathname'),
+    State('home-layout', 'data'),
+    State('FINM350-layout', 'data'),
+    State('FINM367-layout', 'data'),
+)
 
 # Run the app on localhost:8050
 if __name__ == '__main__':
